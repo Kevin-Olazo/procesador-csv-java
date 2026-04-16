4bd3af6f81090a0b98916b9f560e51af160efe0d
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-04-15 20:20:25 -0500

    feat: add Venta class to model sales with properties and subtotal calculation

---
+++ src/main/java/com/gadev/modelo/Venta.java
@@ -0,0 +1,43 @@
+package com.gadev.modelo;
+
+import java.time.LocalDate;
+
+public class Venta {
+    private int id;
+    private LocalDate fecha;
+    private String producto;
+    private int cantidad;
+    private double precioUnitario;
+
+    public Venta(int id, LocalDate fecha, String producto, int cantidad, double precioUnitario) {
+        this.id = id;
+        this.fecha = fecha;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+
+    public double getSubtotal(){
+        return cantidad * precioUnitario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+}
