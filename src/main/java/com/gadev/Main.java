0a291a9b23d52479ad1ee8a2b43fe65978e5d082
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-04-17 00:57:36 -0500

    fix: improve error handling and formatting in ProcesadorCsv methods

--- src/main/java/com/gadev/Main.java
+++ src/main/java/com/gadev/Main.java
@@ -1,7 +1,28 @@
 package com.gadev;
 
+import com.gadev.modelo.Venta;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        ProcesadorCsv procesadorCsv = new ProcesadorCsv();
+
+        String rutaEntrada = "ventas.csv";
+        String rutaSalida = "reporte_final.txt";
+
+        try {
+            // Paso A: Leer el archivo
+            List<Venta> lista = procesadorCsv.cargarVentasDesdeCsv(rutaEntrada);
+            System.out.println("Éxito: Se cargaron " + lista.size() + " transacciones validas.");
+
+            procesadorCsv.imprimirVentas(lista);
+            procesadorCsv.generarReporteResumen(lista, rutaSalida);
+            System.out.println("Reporte generado exitosamente en el archivo: " + rutaSalida);
+        } catch (IOException e) {
+            System.out.println("Error critico de lectura/escritura: " + e.getMessage());
+        }
 
     }
 }
